a187d12eacbd07935e8b6d4e158b05283e3c2dde
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-09 00:00:05 +0530

    added the first level of routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express"
+import authRoutes from '../src/routes/auth.routes.js'
 
 const app = express()
+
+app.use("/api/auth",authRoutes)
 
 app.listen(5001,()=>{
     console.log("server is running on port 5001")
