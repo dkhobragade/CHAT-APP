813013672f27780c99a02bde6276d29c6262749b
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-09 15:13:20 +0530

    added the code for the update profile and check the user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js"
 import { generateToken } from "../lib/utils.js"
 import User from "../models/auth.models.js"
 import bcrypt from 'bcryptjs'
@@ -116,5 +117,40 @@ export const logout =(req,res)=>{
 }
 
 export const updateProfile= async(req,res)=>{
+    try{
+        const {profilePic}=req.body
+        const userId = req.user._id
+
+        if(!profilePic) res.status(400).json({
+            message:'Profile Pic Required'
+        })
+
+        const uplodeResponse = await cloudinary.uploader.uploade(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId,
+            {profilePic:uplodeResponse.secure_url},
+            {new:true}
+        )
+
+        res.status(200).json(updatedUser)
+    }
+    catch(error){
+        console.log("Error while updating Profile Pic",error.message)
+        res.status(500).json({
+            message:'Internal Server Error'
+        })
+
+    }
+}
+
+export const checkAuth = (req,res) => {
+    try{
+        res.status(200).json(req.user)
+    }
+    catch(error){
+        console.log("Error in CheckAuth",error.message)
+        res.status(500).json({
+            message:'Internal Server Error'
+        })
+    }
 
 }
