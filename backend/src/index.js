5065cf309b9fb28d869b835824ed458c31e16195
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-09 12:50:13 +0530

    added the backend and the DB for the singUp

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,15 @@ import authRoutes from '../src/routes/auth.routes.js'
 import { connectDB } from "./lib/db.js"
 import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express()
 const PORT = process.env.PORT
-dotenv.config()
 
 
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 
-
 app.listen(PORT,()=>{
-    console.log("server is running on port 5001")
+    console.log(`server is running on port ${PORT}`)
     connectDB()
 })
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { signUp,login,logout } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
-router.get("/signup",signUp)
+router.post("/signup",signUp)
 
 router.get("/login",login)
 
