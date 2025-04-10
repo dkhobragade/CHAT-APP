826048f8ef13ebe847ea72b2ec5bd32039e6cdab
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-10 13:17:44 +0530

    added the empty pages for all the frontend and install axios for API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Navbar } from '../src/components/Navbar'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+import { ProfilePage } from './pages/ProfilePage'
+import { SettingPage } from './pages/SettingPage'
+import { SignUpPage } from './pages/SignUpPage'
+
 
 const App = () =>
 {
   return (
-    <div className='text-red-600'>App
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/signUp' element={ <SignUpPage /> } />
+        <Route path='/login' element={ <LoginPage /> } />
+        <Route path='/setting' element={ <SettingPage /> } />
+        <Route path='/profile' element={ <ProfilePage /> } />
+      </Routes>
     </div>
   )
 }
