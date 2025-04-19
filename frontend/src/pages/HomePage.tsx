7d6004e1a2940de5aecf287c242be94d341b6a36
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-19 15:02:33 +0530

    Minor clean Up

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useMessageStore } from '../store/useMessageStore'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
