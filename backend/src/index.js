26787487434fbdc7e502e9187c906fe814c17b06
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-20 14:03:37 +0530

    added the sunctionality for the socket.io in the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import dotenv from 'dotenv';
 import cookie_parser from 'cookie-parser'
 import messageRoutes from '../src/routes/message.routes.js'
 import cors from 'cors'
+import {app,server} from '../src/lib/socket.js'
 
 
 dotenv.config()
-const app = express()
 const PORT = process.env.PORT
 
 app.use(cors({
@@ -20,7 +20,7 @@ app.use(cookie_parser())
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
     connectDB()
 })
---
+++ backend/src/lib/socket.js
@@ -0,0 +1,26 @@
+import {Server} from 'socket.io'
+import http from 'http'
+import express from 'express'
+
+
+const app = express()
+const server = http.createServer(app)
+
+
+const io = new Server(server,{
+    cors:{
+        origin:['http://localhost:5173/']
+    }
+})
+
+io.on("Connection",(socket)=>{
+    console.log("A user Connected",socket.id)
+
+    socket.on("disconnect",()=>{
+        console.log("A user disconnect",socket.id)
+    })
+
+})
+
+
+export {io,app,server}
