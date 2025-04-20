fd2a4ee16318345ae83264f88cca5e37ccda8b46
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-20 13:53:27 +0530

    removed the consol log

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,6 @@ export const Navbar = () =>
 {
 
   const { logout, authUser } = useAuthStore()
-  console.log( logout, authUser )
 
   return (
     <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
