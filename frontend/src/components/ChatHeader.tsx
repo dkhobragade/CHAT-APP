5d82f74eed35b30c88819ccaf7faad448cf93b69
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-23 13:38:10 +0530

    minor fix for the aligment issue

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -6,7 +6,7 @@ const ChatHeader = () =>
     const { selectedUser, setSelectedUser } = useMessageStore();
 
     return (
-        <div className="p-2.5 border-b border-base-300">
+        <div className="p-3 border-b border-base-300">
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-3">
                     <div className="avatar">
