bf38e627912bcf24cb0a519df36136f8e02972a8
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-23 13:26:58 +0530

    css changes to make the whole screen for the homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -9,10 +9,10 @@ export const HomePage = () =>
     const { selectedUser } = useMessageStore()
 
     return (
-        <div className='h-screen bg-base-200'>
-            <div className='flex items-center justify-center pt-20 px-4'>
-                <div className='bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]'>
-                    <div className='flex h-full rounded-lg overflow-hidden'>
+        <div className='w-full h-screen bg-base-200'>
+            <div className='flex pt-20 px-4'>
+                <div className='bg-base-100 rounded-lg shadow-xl w-full  h-[calc(105vh-8rem)]'>
+                    <div className='flex w-full h-full rounded-lg overflow-hidden'>
                         <Sidebar />
                         { !selectedUser ? <NoChatSelected /> : <ChatContainer /> }
                     </div>
