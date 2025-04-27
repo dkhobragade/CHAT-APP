a8976a0fd6f91ef12834cd31111b3710571b5a73
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-27 19:56:35 +0530

    added the functionality for the emoji

--- frontend/src/components/MessageInput.tsx
+++ frontend/src/components/MessageInput.tsx
@@ -1,12 +1,14 @@
 import { useRef, useState } from 'react'
 import { useMessageStore } from '../store/useMessageStore'
-import { Image, Send, X } from 'lucide-react'
+import { Annoyed, Image, Send, X } from 'lucide-react'
 import toast from 'react-hot-toast'
+import EmojiPicker, { EmojiClickData } from 'emoji-picker-react'
 
 const MessageInput = () =>
 {
-    const [ text, setText ] = useState( "" )
+    const [ text, setText ] = useState<string>( "" )
     const [ imagePreview, setImagePreview ] = useState<any>( null )
+    const [ isEmojiBoxOpen, setIsEmojiBoxOpen ] = useState<boolean>( false )
     const fileInputRef = useRef<HTMLInputElement>( null )
     const { sendMessages } = useMessageStore()
 
@@ -19,6 +21,7 @@ const MessageInput = () =>
 
     const handleImageChange = ( e: any ) =>
     {
+        setIsEmojiBoxOpen( !isEmojiBoxOpen )
         const file = e.target.files[ 0 ]
         if ( !file.type.startsWith( 'image/' ) )
         {
@@ -33,8 +36,14 @@ const MessageInput = () =>
         reader.readAsDataURL( file )
     }
 
+    const handleEmojiClick = ( emojiData: EmojiClickData, ) =>
+    {
+        setText( ( prev ) => prev + emojiData.emoji )
+    }
+
     const handleSendMessage = async ( e: any ) =>
     {
+        setIsEmojiBoxOpen( !isEmojiBoxOpen )
         e.preventDefault()
         if ( !text.trim() && !imagePreview ) return
 
@@ -56,6 +65,7 @@ const MessageInput = () =>
         }
 
     }
+
 
     return (
         <div className='p-4 w-full'>
@@ -82,7 +92,7 @@ const MessageInput = () =>
             <form onSubmit={ handleSendMessage }>
                 <div className='flex-1 flex gap-2'>
                     <input type="text" className='w-full input input-bordered rounded-lg input-sm sm:input-md' placeholder='Type a message...' value={ text } onChange={ ( e ) => setText( e.target.value ) } />
-
+                    <Annoyed size={ 20 } className='w-10 h-10' color="#e70d0d" strokeWidth={ 1 } onClick={ () => setIsEmojiBoxOpen( !isEmojiBoxOpen ) } />
                     <input type="file" accept='image/*' className='hidden' ref={ fileInputRef } onChange={ handleImageChange } />
 
                     <button
@@ -91,12 +101,13 @@ const MessageInput = () =>
                      ${ imagePreview ? "text-emerald-500" : "text-zinc-400" }` }
                         onClick={ () => fileInputRef.current?.click() }
                     >
-                        <Image size={ 20 } />
+                        <Image size={ 20 } color="#e70d0d" />
                     </button>
-                    <button type='submit' className='btn btn-sm btn-circle' disabled={ !text.trim() && !imagePreview }>
-                        <Send size={ 20 } />
+                    <button type='submit' className='btn btn-circle' disabled={ !text.trim() && !imagePreview }>
+                        <Send size={ 20 } color="#0de74f" />
                     </button>
                 </div>
+                <EmojiPicker onEmojiClick={ handleEmojiClick } className='hidden w-full' style={ { width: '100%', height: '20rem' } } autoFocusSearch={ false } searchDisabled={ true } open={ isEmojiBoxOpen } />
             </form>
 
         </div>
