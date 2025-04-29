65c613fffb84a8f90015b400e96a80cf5443468b
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-29 16:25:21 +0530

    added the feacture of the last seen

--- frontend/src/components/ChatHeader.tsx
+++ frontend/src/components/ChatHeader.tsx
@@ -1,3 +1,4 @@
+import { formatDateToReadable } from '../constants';
 import { useMessageStore } from '../store/useMessageStore';
 import { X } from 'lucide-react';
 
@@ -25,7 +26,7 @@ const ChatHeader = ( { showImg = true, className = 'justify-between' }: Props ) 
     }
 
     return (
-        <div className="p-3 border-b border-base-300">
+        <div className="p-2 border-b border-base-300">
             <div className={ `flex items-center ${ className }` }>
                 { showImg &&
                     <div className="flex items-center gap-3 cursor-pointer" onClick={ onClickSelectedUser }>
@@ -34,7 +35,10 @@ const ChatHeader = ( { showImg = true, className = 'justify-between' }: Props ) 
                                 <img src={ selectedUser.profilePic || "/avatar.png" } alt={ selectedUser.fullName } />
                             </div>
                         </div>
-                        <h3 className="font-medium">{ selectedUser.fullName }</h3>
+                        <div>
+                            <h3 className="font-medium">{ selectedUser.fullName }</h3>
+                            <span className="font-thin text-xs">{ `Last Seen: ${ formatDateToReadable( selectedUser.updatedAt.split( "T" )[ 0 ] ) }` }</span>
+                        </div>
                     </div>
                 }
                 <button className={ className } onClick={ onClickClose }>
--- frontend/src/constants/index.tsx
+++ frontend/src/constants/index.tsx
@@ -34,3 +34,14 @@ const THEMES = [
 ];
 
 export default THEMES
+
+export const formatDateToReadable = ( inputDate: any ) =>
+{
+
+  const date = new Date( inputDate )
+  return date.toLocaleDateString( "en-GB", {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  } )
+}
